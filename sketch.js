ef9e53b8b62da2038467d94299b2a2179d7f88ae
Peter Liley <43123543+petlil@users.noreply.github.com>
2018-10-08 18:21:41 +1300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,9 @@
 //three sliders: age, year, gender
-//draw to the screen a person to show the 'lx' value of
+//draw to the screen a little person to show the 'lx' value of
 //that age group, gender and year
 //(500 people = 1 object)
+//and the little people will make bleeping noises
+//that can accumulate into a 'crowd' sound
 
 //source of the small man image:
 //https://piq.codeus.net/picture/217812/Little-RPG-Person
@@ -19,6 +21,7 @@ function preload(){
   //load the csv file into a table
   //loadTable(filename, file type, top row is headers)
   table = loadTable('/assets/lifeData2.csv', 'csv', 'header');
+  //load two images of little people
   imgF = loadImage('/assets/littlePersonF.png');
   imgM = loadImage('/assets/littlePersonM.png');
 
@@ -28,16 +31,18 @@ function preload(){
 function setup() {
   createCanvas(windowWidth, windowHeight);
   rows = table.getRows();
-
+  
   genderSlider = createSlider(0,1,0);
   genderSlider.position(width/4-100,60);
-
+  
   ageSlider = createSlider(0,100,100);
   ageSlider.position(width/2-100,60);
 
   yearSlider = createSlider(0,107,0);
   yearSlider.position(width*(3/4)-100,60);
-
+  
+  //create an array of blipArray objects and set
+  //parameters (x, y, frequency) and load
   for(var i = 0; i < 1000; i++){
     blipArray[i] = new BlipperCircle;
     blipArray[i].freq = random(100,800);
@@ -55,16 +60,17 @@ function draw() {
   background(201, 228, 202);
   fill(135, 187, 162);
   noStroke();
+  //beautiful aesthetic rectangles 
   rect(0, 0, width, 100);
   rect(0, height-100, width, height);
 
 
-
+  //draw text above sliders that shows what the slider values
+  //are as the user changes them
   fill(54, 73, 88);
   textSize(32);
   text(yearSlider.value()+1876, width*(3/4)-50,50);
   text(ageSlider.value(), width/2-50,50);
-
   textSize(12);
   text('year', width*(3/4)-90,45);
   text('age', width/2-90,45);
@@ -74,19 +80,37 @@ function draw() {
 
   frameRate(60);
   textSize(32);
+  
+  //the way the csv file is structured makes it necessary to do
+  //some maths to navigate to the correct lx value for any
+  //gender, year and age
   if(genderSlider.value() === 0){
+    //there are 606 entries per year
     year = yearSlider.value()*606;
+    //female results come first
     gender = 0;
+    //each year of age has 3 values:
+    //min, max and average.
+    //*3 navigates to the correct age and
+    //+2 navigates to the average
     age = (ageSlider.value()*3)+2;
+    //show the user we're referring to females
     text('female', width/4-80,50);
   }
   else if(genderSlider.value() === 1){
     year = yearSlider.value()*606;
+    //male data begins 303 rows down
+    //each year's segment of the file
+    //hence:
     gender = 303;
     age = (ageSlider.value()*3)+2;
+    //show the user we're referring to males
     text('male', width/4-80,50);
   }
 
+  //add together the various navigational elements to
+  //find and set the lx value to a variable, and round
+  //it to an integer
   var lxValue = (rows[year+age+gender].get('lx'))/500;
   var roundedlx = int(lxValue);
 
@@ -94,9 +118,9 @@ function draw() {
   for(var i = 0; i < roundedlx; i++){
     blipArray[i].play();
   }
+  
   textSize(32);
   text('Number of people: ' + rows[year+age+gender].get('lx'), 10,height-20);
-
   textSize(12);
   text(info, width-600, height-95, 600, 200);
   text('(1 little person represents 500 people)', 10, height-50);
@@ -112,6 +136,8 @@ function BlipperCircle(){
   this.freq;
   this.osc;
   this.adsr;
+  //a little randomness prevents the sounds the people make
+  //from becoming repetative or predictable
   this.randomTimer = int(random(300,400));
   this.randomOffset = int(random(0,400));
 
@@ -129,15 +155,16 @@ function BlipperCircle(){
   }
 
   this.play = function(){
+
     if(genderSlider.value() === 0){
       image(imgF, this.x, this.y);
-
     }
     else{
       image(imgM, this.x, this.y);
 
     }
 
+    //a little cheeky mouse fun
     if(mouseIsPressed && mouseY > 200){
       this.x = this.x + (random(-3,3));
       this.y = this.y + (random(-3,3));
@@ -146,15 +173,11 @@ function BlipperCircle(){
       this.x = this.x + (random(-1,1));
       this.y = this.y + (random(-1,1));
     }
-
+    
     if((frameCount+this.randomOffset)%this.randomTimer === 0){
       this.osc.start();
       this.adsr.play(this.osc, 0, 0.2);
     }
 
   }
-}
-
-function mousePressed(){
-
 }
